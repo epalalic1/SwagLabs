b56e6d6cbfc6b6a5dd7115e7ca727588cd525251
Emina Palalic <epalalic1@etf.unsa.ba>
2024-10-07 14:54:55 +0200

    Create login tests

--- swaglabs/src/test/java/steps/LoginSteps.java
+++ swaglabs/src/test/java/steps/LoginSteps.java
@@ -21,17 +21,48 @@ public class LoginSteps {
         driver = new ChromeDriver();
         driver.get("https://www.saucedemo.com/");
     }
+    
     @When("I enter valid credentials")
     public void i_enter_valid_credentials() {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector(".btn_action")).click();
     }
+
+
+    @When("I click on login button")
+    public void i_click_on_login_button() {
+        driver.findElement(By.cssSelector(".btn_action")).click();
+    }
+
+    @When("I enter username {string}")
+    public void  i_enter_username(String username) {
+        driver.findElement(By.id("user-name")).sendKeys(username);
+    }
+
+    @When("I enter password {string}")
+    public void i_enter_password(String password) {
+        driver.findElement(By.id("password")).sendKeys(password);
+    }
+
+    @When("I enter invalid credentials")
+    public void i_enter_invalid_credentials() {
+        driver.findElement(By.id("user-name")).sendKeys("inavalid_user_username");
+        driver.findElement(By.id("password")).sendKeys("invalid_user_password");  
+    }
+
     @Then("I should be redirected to the homepage")
     public void i_should_be_redirected_to_the_homepage() {
         String expectedUrl = "https://www.saucedemo.com/inventory.html";
         String actualUrl = driver.getCurrentUrl();
         assertEquals(expectedUrl, actualUrl);
+        driver.quit();
+    }
+
+    @Then("I should see error message {string}")
+    public void i_should_see_error_message(String errorMessage) {
+        String actualMessage = driver.findElement(By.cssSelector("h3[data-test='error']")).getText();
+        assertEquals(errorMessage, actualMessage);
         driver.quit();
     }
 
--- swaglabs/target/test-classes/resources/login.feature
+++ swaglabs/target/test-classes/resources/login.feature
@@ -4,3 +4,42 @@ Feature: Login Functionality
     Given I am on the login page
     When I enter valid credentials
     Then I should be redirected to the homepage
+
+  Scenario: Unsuccessful login - empty username and password
+    Given I am on the login page
+    When I click on login button
+    Then I should see error message "Epic sadface: Username is required"
+
+  Scenario: Unsuccessful login - empty password
+    Given I am on the login page
+    When I enter username "standard_user"
+    When I click on login button
+    Then I should see error message "Epic sadface: Password is required"
+
+  Scenario: Unsuccessful login - empty username
+    Given I am on the login page
+    When I enter password "secret_sauce"
+    When I click on login button
+    Then I should see error message "Epic sadface: Username is required"
+
+  Scenario: Unsuccesful login - invalid credentials
+    Given I am on the login page
+    When I enter invalid credentials
+    When I click on login button
+    Then I should see error message "Epic sadface: Username and password do not match any user in this service"
+
+  Scenario: Unsuccesful login - invalid username
+    Given I am on the login page
+    When I enter username "invalid_username"
+    When I enter password "secret_sauce"
+    When I click on login button
+    Then I should see error message "Epic sadface: Username and password do not match any user in this service"
+  
+  Scenario: Unsuccesful login - invalid password
+    Given I am on the login page
+    When I enter username "standard_user"
+    When I enter password "invalid_password"
+    When I click on login button
+    Then I should see error message "Epic sadface: Username and password do not match any user in this service"
+
+  
